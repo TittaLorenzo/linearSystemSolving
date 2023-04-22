7c2feb9bc13be16733a8206643d2bb1e498ef2da
Gabriele Lecchi <gabriele.lecchi@gmail.com>
2023-04-22 19:05:45 +0200

    Aggiunta parte jacobi alla relazione

--- lslibrary/src/main/java/com/lsproject/App.java
+++ lslibrary/src/main/java/com/lsproject/App.java
@@ -13,103 +13,101 @@ public class App {
     public static void main(String[] args) throws IOException {
 
         // JACOBI
-        // /*
-        //  * LSSolver lss = new Jacobi("lslibrary/src/main/java/com/Matrici/spa1.mtx");
-        //  * System.out.println(
-        //  * "____________________________________________________________________________________________________"
-        //  * );
-        //  * System.out.println();
-        //  * System.out.println("JACOBI eseguito su spa1.mtx");
-        //  * System.out.println();
-        //  * lss.executeMethods();
-        //  * 
-        //  * lss = new Jacobi("lslibrary/src/main/java/com/Matrici/spa2.mtx");
-        //  * System.out.println(
-        //  * "____________________________________________________________________________________________________"
-        //  * );
-        //  * System.out.println();
-        //  * System.out.println("JACOBI eseguito su spa2.mtx");
-        //  * System.out.println();
-        //  * lss.executeMethods();
-        //  * 
-        //  * lss = new Jacobi("lslibrary/src/main/java/com/Matrici/vem1.mtx");
-        //  * System.out.println(
-        //  * "____________________________________________________________________________________________________"
-        //  * );
-        //  * System.out.println();
-        //  * System.out.println("JACOBI eseguito su vem1.mtx");
-        //  * System.out.println();
-        //  * lss.executeMethods();
-        //  * 
-        //  * lss = new Jacobi("lslibrary/src/main/java/com/Matrici/vem2.mtx");
-        //  * System.out.println(
-        //  * "____________________________________________________________________________________________________"
-        //  * );
-        //  * System.out.println();
-        //  * System.out.println("JACOBI eseguito su vem2.mtx");
-        //  * System.out.println();
-        //  * lss.executeMethods();
-        //  * System.out.println(
-        //  * "____________________________________________________________________________________________________"
-        //  * );
-        //  */
-        // //
 
-        // // GAUSS SEIDEL
-        // /*
-        //  * LSSolver lss = new
-        //  * Graubseidel("lslibrary/src/main/java/com/Matrici/spa1.mtx");
-        //  * System.out.println(
-        //  * "____________________________________________________________________________________________________"
-        //  * );
-        //  * System.out.println();
-        //  * System.out.println("GAUSS SEIDEL eseguito su spa1.mtx");
-        //  * System.out.println();
-        //  * lss.executeMethods();
-        //  * 
-        //  * lss = new Graubseidel("lslibrary/src/main/java/com/Matrici/spa2.mtx");
-        //  * System.out.println(
-        //  * "____________________________________________________________________________________________________"
-        //  * );
-        //  * System.out.println();
-        //  * System.out.println("GAUSS SEIDEL eseguito su spa2.mtx");
-        //  * System.out.println();
-        //  * lss.executeMethods();
-        //  * 
-        //  * lss = new Graubseidel("lslibrary/src/main/java/com/Matrici/vem1.mtx");
-        //  * System.out.println(
-        //  * "____________________________________________________________________________________________________"
-        //  * );
-        //  * System.out.println();
-        //  * System.out.println("GAUSS SEIDEL eseguito su vem1.mtx");
-        //  * System.out.println();
-        //  * lss.executeMethods();
-        //  * 
-        //  * lss = new Graubseidel("lslibrary/src/main/java/com/Matrici/vem2.mtx");
-        //  * System.out.println(
-        //  * "____________________________________________________________________________________________________"
-        //  * );
-        //  * System.out.println();
-        //  * System.out.println("GAUSS SEIDEL eseguito su vem2.mtx");
-        //  * System.out.println();
-        //  * lss.executeMethods();
-        //  */
-
-        //GRADIENTE
-
-        LSSolver lss = new Gradiente("lslibrary/src/main/java/com/Matrici/spa2.mtx");
+        LSSolver lss = new Jacobi("lslibrary/src/main/java/com/Matrici/spa1.mtx");
         System.out.println(
                 "____________________________________________________________________________________________________");
         System.out.println();
-        System.out.println("GRADIENTE eseguito su spa1.mtx");
+        System.out.println("JACOBI eseguito su spa1.mtx");
         System.out.println();
         lss.executeMethods();
 
-        //  //GRADIENTE CONIUGATO
+        lss = new Jacobi("lslibrary/src/main/java/com/Matrici/spa2.mtx");
+        System.out.println(
+                "____________________________________________________________________________________________________");
+        System.out.println();
+        System.out.println("JACOBI eseguito su spa2.mtx");
+        System.out.println();
+        lss.executeMethods();
 
-        // LSSolver lss = new Gradienteconiugato("lslibrary/src/main/java/com/Matrici/spa1.mtx");
+        lss = new Jacobi("lslibrary/src/main/java/com/Matrici/vem1.mtx");
+        System.out.println(
+                "____________________________________________________________________________________________________");
+        System.out.println();
+        System.out.println("JACOBI eseguito su vem1.mtx");
+        System.out.println();
+        lss.executeMethods();
+
+        lss = new Jacobi("lslibrary/src/main/java/com/Matrici/vem2.mtx");
+        System.out.println(
+                "____________________________________________________________________________________________________");
+        System.out.println();
+        System.out.println("JACOBI eseguito su vem2.mtx");
+        System.out.println();
+        lss.executeMethods();
+        System.out.println(
+                "____________________________________________________________________________________________________");
+
+        // GAUSS SEIDEL
+        // /*
+        // * LSSolver lss = new
+        // * Graubseidel("lslibrary/src/main/java/com/Matrici/spa1.mtx");
+        // * System.out.println(
+        // *
+        // "____________________________________________________________________________________________________"
+        // * );
+        // * System.out.println();
+        // * System.out.println("GAUSS SEIDEL eseguito su spa1.mtx");
+        // * System.out.println();
+        // * lss.executeMethods();
+        // *
+        // * lss = new Graubseidel("lslibrary/src/main/java/com/Matrici/spa2.mtx");
+        // * System.out.println(
+        // *
+        // "____________________________________________________________________________________________________"
+        // * );
+        // * System.out.println();
+        // * System.out.println("GAUSS SEIDEL eseguito su spa2.mtx");
+        // * System.out.println();
+        // * lss.executeMethods();
+        // *
+        // * lss = new Graubseidel("lslibrary/src/main/java/com/Matrici/vem1.mtx");
+        // * System.out.println(
+        // *
+        // "____________________________________________________________________________________________________"
+        // * );
+        // * System.out.println();
+        // * System.out.println("GAUSS SEIDEL eseguito su vem1.mtx");
+        // * System.out.println();
+        // * lss.executeMethods();
+        // *
+        // * lss = new Graubseidel("lslibrary/src/main/java/com/Matrici/vem2.mtx");
+        // * System.out.println(
+        // *
+        // "____________________________________________________________________________________________________"
+        // * );
+        // * System.out.println();
+        // * System.out.println("GAUSS SEIDEL eseguito su vem2.mtx");
+        // * System.out.println();
+        // * lss.executeMethods();
+        // */
+
+        // GRADIENTE
+
+        // LSSolver lss = new Gradiente("lslibrary/src/main/java/com/Matrici/spa2.mtx");
         // System.out.println(
-        //         "____________________________________________________________________________________________________");
+        // "____________________________________________________________________________________________________");
+        // System.out.println();
+        // System.out.println("GRADIENTE eseguito su spa1.mtx");
+        // System.out.println();
+        // lss.executeMethods();
+
+        // //GRADIENTE CONIUGATO
+
+        // LSSolver lss = new
+        // Gradienteconiugato("lslibrary/src/main/java/com/Matrici/spa1.mtx");
+        // System.out.println(
+        // "____________________________________________________________________________________________________");
         // System.out.println();
         // System.out.println("GRADIENTE CONIUGATO eseguito su spa1.mtx");
         // System.out.println();
@@ -117,7 +115,7 @@ public class App {
 
         // lss = new Gradienteconiugato("lslibrary/src/main/java/com/Matrici/spa2.mtx");
         // System.out.println(
-        //         "____________________________________________________________________________________________________");
+        // "____________________________________________________________________________________________________");
         // System.out.println();
         // System.out.println("GRADIENTE CONIUGATO eseguito su spa2.mtx");
         // System.out.println();
@@ -125,7 +123,7 @@ public class App {
 
         // lss = new Gradienteconiugato("lslibrary/src/main/java/com/Matrici/vem1.mtx");
         // System.out.println(
-        //         "____________________________________________________________________________________________________");
+        // "____________________________________________________________________________________________________");
         // System.out.println();
         // System.out.println("GRADIENTE CONIUGATO eseguito su vem1.mtx");
         // System.out.println();
@@ -133,7 +131,7 @@ public class App {
 
         // lss = new Gradienteconiugato("lslibrary/src/main/java/com/Matrici/vem2.mtx");
         // System.out.println(
-        //         "____________________________________________________________________________________________________");
+        // "____________________________________________________________________________________________________");
         // System.out.println();
         // System.out.println("GRADIENTE CONIUGATO eseguito su vem2.mtx");
         // System.out.println();
